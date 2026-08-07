1ff395816b30fde2ae50e64dbbb7937677a3ed47
cesar <cealfarias@gmail.com>
2026-08-07 15:19:39 -0600

    Primera versi├│n SaaS Multiempresa

---
+++ des.bat
@@ -0,0 +1,8 @@
+cd c:\planilla\version_web
+git init
+git add .
+git commit -m "Primera versión SaaS Multiempresa"
+git branch -M main
+# Reemplaza la URL de abajo con la que te dé GitHub
+git remote add origin https://github.com/cealfarias/planilla.git
+git push -u origin main
